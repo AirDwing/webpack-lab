312497bb9658eed821606c51566e79b1451fc5fc
willin <willin@willin.org>
2017-08-04 08:46:39 +0800

    eslint react add

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,12 @@
 
 module.exports = {
   extends: [
-    'dwing'
+    'eslint-config-dwing',
+    'eslint-config-airbnb-base/rules/strict',
+    'eslint-config-airbnb/rules/react'
+  ].map(require.resolve),
+  plugins: [
+    'react'
   ],
   parser: 'babel-eslint',
   env: {
